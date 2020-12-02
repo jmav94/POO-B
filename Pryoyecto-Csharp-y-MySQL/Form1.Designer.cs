769bf29eab36fe938336c7e412f541679c3bb6ec
Juan Ahumada <juan.ahumada94@gmail.com>
2020-12-02 11:58:30 -0600

    Avances de proyecto con MySQL

---
+++ Pryoyecto-Csharp-y-MySQL/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Pryoyecto_Csharp_y_MySQL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbDatos = new System.Windows.Forms.GroupBox();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAdress = new System.Windows.Forms.TextBox();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.btnUpdateList = new System.Windows.Forms.Button();
+            this.lbUsuarios = new System.Windows.Forms.ListBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.gbDatos.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbDatos
+            // 
+            this.gbDatos.Controls.Add(this.btnUpdateList);
+            this.gbDatos.Controls.Add(this.btnRegister);
+            this.gbDatos.Controls.Add(this.label3);
+            this.gbDatos.Controls.Add(this.txtAdress);
+            this.gbDatos.Controls.Add(this.label2);
+            this.gbDatos.Controls.Add(this.txtLastName);
+            this.gbDatos.Controls.Add(this.label1);
+            this.gbDatos.Controls.Add(this.txtFirstName);
+            this.gbDatos.Location = new System.Drawing.Point(12, 12);
+            this.gbDatos.Name = "gbDatos";
+            this.gbDatos.Size = new System.Drawing.Size(198, 202);
+            this.gbDatos.TabIndex = 0;
+            this.gbDatos.TabStop = false;
+            this.gbDatos.Text = "Datos";
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(6, 36);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(100, 20);
+            this.txtFirstName.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "First name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Last name";
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(6, 82);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(100, 20);
+            this.txtLastName.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Address";
+            // 
+            // txtAdress
+            // 
+            this.txtAdress.Location = new System.Drawing.Point(6, 128);
+            this.txtAdress.Name = "txtAdress";
+            this.txtAdress.Size = new System.Drawing.Size(186, 20);
+            this.txtAdress.TabIndex = 4;
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(6, 164);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(75, 23);
+            this.btnRegister.TabIndex = 6;
+            this.btnRegister.Text = "Register User";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // btnUpdateList
+            // 
+            this.btnUpdateList.Location = new System.Drawing.Point(87, 164);
+            this.btnUpdateList.Name = "btnUpdateList";
+            this.btnUpdateList.Size = new System.Drawing.Size(105, 23);
+            this.btnUpdateList.TabIndex = 7;
+            this.btnUpdateList.Text = "Update User List";
+            this.btnUpdateList.UseVisualStyleBackColor = true;
+            // 
+            // lbUsuarios
+            // 
+            this.lbUsuarios.FormattingEnabled = true;
+            this.lbUsuarios.Location = new System.Drawing.Point(216, 12);
+            this.lbUsuarios.Name = "lbUsuarios";
+            this.lbUsuarios.Size = new System.Drawing.Size(253, 199);
+            this.lbUsuarios.TabIndex = 1;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(475, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(224, 199);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(857, 235);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.lbUsuarios);
+            this.Controls.Add(this.gbDatos);
+            this.Name = "Form1";
+            this.Text = "Usuarios";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.gbDatos.ResumeLayout(false);
+            this.gbDatos.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbDatos;
+        private System.Windows.Forms.Button btnUpdateList;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAdress;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.ListBox lbUsuarios;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ Pryoyecto-Csharp-y-MySQL/Form1.cs
@@ -0,0 +1,105 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pryoyecto_Csharp_y_MySQL
+{
+    public partial class Form1 : Form
+    {
+        static string ConnectionString = "datasource = localhost; port= 3306; username = root; password =;database = test";
+        static MySqlConnection databaseConnection = new MySqlConnection(ConnectionString);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                
+                databaseConnection.Open();
+                MessageBox.Show("Conexion a base de datos establecida con exito.");
+                databaseConnection.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+
+        }
+        private void SaveUser()
+        {
+            string query = $"INSERT INTO `user` (`id`, `first_name`, `last_name`, `address`) VALUES (NULL, '{txtFirstName.Text}', '{txtLastName.Text}', '{txtAdress.Text}')";
+            
+            MySqlCommand commandDatabase = new MySqlCommand(query,databaseConnection);
+            commandDatabase.CommandTimeout = 60;
+
+            try
+            {
+                databaseConnection.Open();
+
+                MySqlDataReader myReader = commandDatabase.ExecuteReader();
+
+                MessageBox.Show("Usario insertado satisfatoriamente!");
+
+                databaseConnection.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void ListUsers()
+        {
+            string ConnectionString = "datasource = localhost; port= 3306; username = root; password =;database = test";
+            string query = "SELECT * FROM user";
+
+            MySqlConnection databaseConnection = new MySqlConnection(ConnectionString);
+            MySqlCommand commandDatabase = new MySqlCommand(query);
+            commandDatabase.CommandTimeout = 60;
+            MySqlDataReader reader;
+
+            try
+            {
+                databaseConnection.Open();
+                reader = commandDatabase.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader.GetString(0)} - {reader.GetString(1)} - {reader.GetString(2)} - {reader.GetString(3)}");
+
+                        string[] row = { reader.GetString(0), reader.GetString(1), reader.GetString(2), reader.GetString(3), };
+
+                        var ListViewItem = new ListViewItem(row);
+                        listView1.Items.Add(ListViewItem);
+                    }
+                }
+
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            SaveUser();
+//            ListUsers();
+        }
+    }
+}
